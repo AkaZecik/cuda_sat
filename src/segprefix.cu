0fdd6314c9d425ed40ad8ac14177ffdc17921e83
Artur Zubilewicz <artur.zubilewicz@gmail.com>
2019-02-16 11:34:40 +0100

    Create defines and a shape of sat_kernel

--- src/segprefix.cu
+++ src/segprefix.cu
@@ -1,13 +1,6 @@
 #include <cstdio>
 #include "segprefix.h"
 
-#define l1(n) ((n & 0xff000000) >> 24)
-#define l2(n) ((n & 0x00ff0000) >> 16)
-#define l3(n) ((n & 0x0000ff00) >> 8)
-#define v1(n) (((n & 0x000000ff) >> 7) & 1)
-#define v2(n) (((n & 0x000000ff) >> 6) & 1)
-#define v3(n) (((n & 0x000000ff) >> 5) & 1)
-
 #define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
 inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true) {
 	if (code != cudaSuccess) {
@@ -15,6 +8,101 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=t
 		if (abort) exit(code);
 	}
 }
+
+// lX - literal X
+// vX - value of literal X
+// sX - sign of literal X
+#define l1(n) (((n) & 0xff000000u) >> 24)
+#define l2(n) (((n) & 0x00ff0000u) >> 16)
+#define l3(n) (((n) & 0x0000ff00u) >> 8)
+#define v1(n) (((n) & 0x00000080u) >> 7)
+#define v2(n) (((n) & 0x00000040u) >> 6)
+#define v3(n) (((n) & 0x00000020u) >> 5)
+#define s1(n) (((n) & 0x00000010u) >> 4)
+#define s2(n) (((n) & 0x00000008u) >> 3)
+#define s3(n) (((n) & 0x00000004u) >> 2)
+
+#define unset_l1(n) ((n) & 0x00ffffffu)
+#define unset_l2(n) ((n) & 0xff00ffffu)
+#define unset_l3(n) ((n) & 0xffff00ffu)
+#define unset_v1(n) ((n) & 0xffffff7fu)
+#define unset_v2(n) ((n) & 0xffffffbfu)
+#define unset_v3(n) ((n) & 0xffffffdfu)
+#define unset_s1(n) ((n) & 0xffffffefu)
+#define unset_s2(n) ((n) & 0xfffffff7u)
+#define unset_s3(n) ((n) & 0xfffffffbu)
+
+#define set_l1(n, v) (unset_l1(n) | (((v) & 0x000000ffu) << 24))
+#define set_l2(n, v) (unset_l2(n) | (((v) & 0x000000ffu) << 16))
+#define set_l3(n, v) (unset_l3(n) | (((v) & 0x000000ffu) << 8))
+#define set_v1(n) ((n) | 0x00000080u)
+#define set_v2(n) ((n) | 0x00000040u)
+#define set_v3(n) ((n) | 0x00000020u)
+#define set_s1(n) ((n) | 0x00000010u)
+#define set_s2(n) ((n) | 0x00000008u)
+#define set_s3(n) ((n) | 0x00000004u)
+
+typedef unsigned int uint;
+typedef unsigned char uchar;
+
+__global__ void sat_kernel(uint *d_arr, uint *d_valid, int s, int k, int n, int r) {
+	int warps_nb = (blockDim.x + 31) >> 5;
+	int warp_id = warps_nb * blockIdx.x + (threadIdx.x >> 5);
+	int formula_id = warp_id / 3;
+	int copy_id = warp_id - 3 * formula_id;
+	uint *formula = d_arr + formula_id * (n + r);
+	uint *assignments = formula + r;
+	uint first_clause = formula[0];
+	int lit1 = l1(first_clause);
+	int lit2 = l2(first_clause);
+	int lit3 = l3(first_clause);
+
+	if(copy_id == 0) {
+		if(a == 0) {
+			return;
+		}
+
+		for(int i = threadIdx.x & 31; i < r; i += 32) {
+			uint clause = formula[i];
+		}
+	}
+
+	if(copy_id == 1) {
+		if(b == 0) {
+			return;
+		}
+
+		for(int i = threadIdx.x & 31; i < r; i += 32) {
+			uint clause = formula[i];
+		}
+	}
+
+	if(copy_id == 2) {
+		if(c == 0) {
+			return;
+		}
+
+		for(int i = threadIdx.x & 31; i < r; i += 32) {
+			uint clause = formula[i];
+		}
+	}
+}
+
+__global__ void 1d_scan() {
+
+}
+
+__global__ void 2d_scan() {
+
+}
+
+int main() {
+	int n;
+	int r;
+	int s;
+}
+
+////////////////////////////////////////////////////////////////////////
 
 struct pair {
 	int v;
